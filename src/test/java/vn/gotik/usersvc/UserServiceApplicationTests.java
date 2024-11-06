be596fbdef1886850c8c7714b39dfea48834a079
Khoi Nguyen Bao <nbkhoi@tma.com.vn>
2024-11-06 21:31:55 +0700

    init commit

---
+++ src/test/java/vn/gotik/usersvc/UserServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package vn.gotik.usersvc;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
